1ac90e7922d17a72612e4189ee8caa116dc2c112
joeld42 <joeld42@gmail.com>
2023-01-26 14:45:01 -0800

    Initial morph test

---
+++ fonts/makefont.bat
@@ -0,0 +1,1 @@
+c:\Toolkits\msdf-atlas-gen\msdf-atlas-gen.exe -type sdf -emrange 0.5 -font "fonts\arco\ARCO for OSX.otf" -charset fonts\charsets.txt -format png -pots -imageout fonts\arco.png -json fonts\arco.json
