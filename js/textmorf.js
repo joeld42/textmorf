18209e9078da0d91e7b82fbb87123fd2caee590c
Joel Davis <joeld42@gmail.com>
2023-02-10 17:21:50 -0800

    WIP on text morphs

--- js/textmorf.js
+++ js/textmorf.js
@@ -25,6 +25,8 @@ void main() {
 `
 
 const glyphFragmentSource = `
+  #extension GL_OES_standard_derivatives : enable
+
     precision mediump float;
         
     uniform vec2 resolution;
@@ -33,6 +35,11 @@ const glyphFragmentSource = `
 
     uniform vec4 u_glyphRectA; // glyph Rect in screen space
     uniform vec4 u_glyphAtlasA; // glyph STs in atlas
+    uniform float u_glyphBiasA;
+
+    uniform vec4 u_glyphRectB; 
+    uniform vec4 u_glyphAtlasB;
+    uniform float u_glyphBiasB;
 
     uniform vec4 fillColor;
     uniform vec4 strokeColor;
@@ -51,34 +58,58 @@ const glyphFragmentSource = `
       return vec4( source.rgb * source.a + backdrop.rgb * backdrop.a * (1. - source.a),
                    source.a  + backdrop.a * (1. - source.a));
     }
+
+
     
 
     void main() {
 
+      // A character
       vec2 glyphST_a = vec2( (pos.x - u_glyphRectA.x) / (u_glyphRectA.z - u_glyphRectA.x),
                              (pos.y - u_glyphRectA.y) / (u_glyphRectA.w - u_glyphRectA.y) ) ;
     
       float activeA = min( 1.0-step( glyphST_a.x, 0.0 ), step( glyphST_a.x, 1.0 )) *
                       min( 1.0-step( glyphST_a.y, 0.0 ), step( glyphST_a.y, 1.0 ));
-
       
       // expand the ST to the atlased ST
       vec2 atlasST_a = vec2( u_glyphAtlasA.x + glyphST_a.x * u_glyphAtlasA.z,
                              u_glyphAtlasA.y + glyphST_a.y * u_glyphAtlasA.w );
-      vec4 sdf = texture2D( u_fonttex, atlasST_a );
-      float d = 0.66 - sdf.x;      
-      float glyphFill = smoothstep( -0.03, 0.03,  -d ) * activeA;
-      float glyphStroke = (1.0 - smoothstep( 0.03, 0.05, abs(d) )) * activeA;
+      vec4 sdfA = texture2D( u_fonttex, atlasST_a ) * activeA;
+
+      // B character
+      vec2 glyphST_b = vec2( (pos.x - u_glyphRectB.x) / (u_glyphRectB.z - u_glyphRectB.x),
+                             (pos.y - u_glyphRectB.y) / (u_glyphRectB.w - u_glyphRectB.y) ) ;
+    
+      float activeB = min( 1.0-step( glyphST_b.x, 0.0 ), step( glyphST_b.x, 1.0 )) *
+                      min( 1.0-step( glyphST_b.y, 0.0 ), step( glyphST_b.y, 1.0 ));
+      
+      // expand the ST to the atlased ST
+      vec2 atlasST_b = vec2( u_glyphAtlasB.x + glyphST_b.x * u_glyphAtlasB.z,
+                             u_glyphAtlasB.y + glyphST_b.y * u_glyphAtlasB.w );
+      vec4 sdfB = texture2D( u_fonttex, atlasST_b ) * activeB;
+      
+      
+      float d = smin( (0.66 + u_glyphBiasA) - sdfA.r, 
+                      (0.66 + u_glyphBiasB) - sdfB.r, 0.2 );
+      
+      float glyphFill = smoothstep( -0.03, 0.03,  -d );
+      
+      float dd = fwidth(d);
+      float glyphStroke = (1.0 - smoothstep( 0.03 -dd, 0.03, abs(d) ));
+      //float glyphStroke = (1.0 - smoothstep( 0.03, 0.05, abs(d) ));
 
       vec4 glyphColor = over( vec4( strokeColor.rgb, strokeColor.a * glyphStroke),
                           vec4( fillColor.rgb, fillColor.a * glyphFill));
-                    
+                      
+       //vec4 bg =  mix( vec4(1,0,0,1), vec4( atlasST_b.x, atlasST_b.y, 0.5, 1.0) , activeB );                          
        gl_FragColor = glyphColor;
     }
 `
 
 const m4 = twgl.m4;
 const gl = document.getElementById("textcanvas").getContext("webgl");
+gl.getExtension("OES_standard_derivatives");
+
 const programInfo = twgl.createProgramInfo(gl, [ glyphVertexSource, glyphFragmentSource ]);
 const glyphs = {}
 const arrays = {
@@ -97,12 +128,40 @@ const textures = twgl.createTextures( gl, {
           }   
       });
 
+// draw a single glyph
 function drawGlyph( x, y, size, gg, uniforms ) {
     
   uniforms.u_glyphPos = [ x, y, x + size, y + size];
   uniforms.u_glyphRectA = [ x, y, x + size, y + size ];
-
+  uniforms.u_glyphBiasA = 0.0;
   uniforms.u_glyphAtlasA = gg.atlas;
+
+  uniforms.u_glyphBiasB = 1;
+
+  twgl.setUniforms(programInfo, uniforms);
+  twgl.drawBufferInfo(gl, bufferInfo);
+
+}
+
+// draw two morphing glyphs
+function drawMorphGlyph( xA, yA, ggA, wA,
+                         xB, yB, ggB, wB,
+                         size, uniforms ) {
+    
+  uniforms.u_glyphPos = [ Math.min(xA, xB), 
+                          Math.min( yA, yB), 
+                          Math.max( xA, xB) + size, 
+                          Math.max( yA, yB) + size ];
+
+  uniforms.u_glyphRectA = [ xA, yA, xA + size, yA + size ];
+  uniforms.u_glyphRectB = [ xB, yB, xB + size, yB + size ];
+
+  uniforms.u_glyphAtlasA = ggA.atlas;
+  uniforms.u_glyphAtlasB = ggB.atlas;
+
+  uniforms.u_glyphBiasA = wA * 0.3;
+  uniforms.u_glyphBiasB = wB * 0.3;
+  //console.log( "wA wB ", wA, wB );
 
   twgl.setUniforms(programInfo, uniforms);
   twgl.drawBufferInfo(gl, bufferInfo);
@@ -141,13 +200,20 @@ function render(time) {
   //twgl.setUniforms(programInfo, uniforms);
   //twgl.drawBufferInfo(gl, bufferInfo);
 
-  let gx = -8 + (Math.sin( time * 0.001) * 3);
+  // let gx = -8 + (Math.sin( time * 0.001) * 3);
+  let gx = -16.0;
   let gy = Math.sin( time * 0.0001) * 2 - 1;  
   
   //drawGlyph( gx, gy, glyphs[69], uniforms ); // H  
-  
+
   const fontsize = 8.0;
-  const str = "HELLO\uD83D\uDE00\u0278";
+  let blend = (Math.sin( time * 0.001) + 1.0) / 2.0;
+  drawMorphGlyph( gx, gy, glyphs[72], blend,
+                  gx, gy, glyphs[77], 1.0 - blend,
+                  fontsize, uniforms );  
+  gx += fontsize  * glyphs[72].advance * 0.8;
+  
+  const str = "ELLO\uD83D\uDE00\u0278";
   const iterator = str[Symbol.iterator]();
   let theChar = iterator.next();
 
@@ -166,8 +232,9 @@ function render(time) {
       // need to convert these to UTF16 or something?
       //console.log("multibyte chars not supported yet");
     }
-    theChar = iterator.next();          
+    theChar = iterator.next();       
   }
+  
   
 
 
@@ -176,7 +243,7 @@ function render(time) {
 
 xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("demo").innerHTML = this.responseText;
+        //document.getElementById("demo").innerHTML = this.responseText;
         var data=this.responseText;
         var jsondata = JSON.parse(data);
 
